b041b0394e7b2df23b875375736f4e90c0fbbd68
Willian Ozako <willian.ozako@gmail.com>
2023-09-05 09:09:18 -0300

    Add comments

--- src/main.go
+++ src/main.go
@@ -7,22 +7,42 @@ import (
 	"github.com/gofiber/template/html/v2"
 )
 
-type Person struct {
-	Name string
+type Comment struct {
+	Title string
+	Body  string
 }
+
+type Storage struct {
+	Comments []Comment
+}
+
+var comments = []Comment{
+	{
+		"Comment #1",
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	},
+	{
+		"Comment #2",
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	},
+	{
+		"Comment #3",
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	},
+}
+
+var storage = Storage{comments}
 
 func main() {
 
 	engine := html.New("./views", ".html")
-
-	bob := Person{"Bob"}
 
 	app := fiber.New(fiber.Config{
 		Views: engine,
 	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Render("index", bob)
+		return c.Render("index", storage)
 	})
 
 	log.Fatal(app.Listen(":3000"))
